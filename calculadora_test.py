2ffa16ccdd43304971769180636d4f90141e2c2d
Fran_27 <27francis3@gmail.com>
2025-11-02 20:58:27 -0600

    Ya se logro, pero creo que puedo ponerlo con input

--- calculadora_test.py
+++ calculadora_test.py
@@ -2,12 +2,24 @@ from metodo_test import calculadora
 
 igual = calculadora()
 
-
+a=5
+b=4
 
 suma = igual.sum(a,b)
+def test_suma():
+    assert suma == 10
+
 resta = igual.res(a,b)
+def test_resta():
+    assert resta == 0
+
 multiplicacion = igual.mul(a,b)
+def test_mul():
+    assert multiplicacion == 25
+
 division = igual.div(a,b)
+def test_div():
+    assert division == 1.0
 
 
 print("La suma de sus numeros es: ", suma)
